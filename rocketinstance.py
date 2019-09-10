42a10720d4aa130137df1024519b94a307dee9ae
Joe Gallagher <joedgallagher@yahoo.com>
2019-09-10 14:35:25 -0400

    very basic motion and gravity

--- rocketinstance.py
+++ rocketinstance.py
@@ -4,10 +4,21 @@ from pygame.locals import *
 class rocket:
     def __init__(self):
         self.BLUE = (0,0,255)
+        self.RED = (255,0,0)
+        self.color = None
+        self.xpos = 300
+        self.ypos = 0
+        self.vspeed = 0
         print('Rocket exists!')
 
     def onloop(self):
         print('Rocket still exists!')
+        self.vspeed = self.vspeed+0.002
+        self.ypos = self.ypos+self.vspeed
 
     def draw(self, surface):
-        pygame.draw.rect(surface, self.BLUE,(200,200,20,199))
+        if self.ypos>400:
+            self.color = self.RED;
+        else:
+            self.color = self.BLUE;
+        pygame.draw.rect(surface, self.color,(self.xpos,self.ypos,20,199))
